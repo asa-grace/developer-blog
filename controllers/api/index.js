c8ffe7fc108a2393e5d68ad393a800e838c219b8
Asa Grace <asag182@gmail.com>
2021-09-27 23:58:43 -0600

    homepage

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,7 +2,9 @@ const router = require('express').Router();
 
 const userRoutes = require('./user-routes');
 const commentRoutes = require('./comment-routes');
+const postRoutes = require('./comment-routes');
 
+router.use('/posts', postRoutes);
 router.use('/users', userRoutes);
 router.use('/comments', commentRoutes);
 
--- server.js
+++ server.js
@@ -1,38 +1,37 @@
 const path = require('path');
 const express = require('express');
-app.use(require('./controllers/'));
 const sequelize = require("./config/connection");
 
 // const session = require('express-session');
-// const exphbs = require('express-handlebars');
+const exphbs = require('express-handlebars');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-const sess = {
-    secret: 'SuperSecret',
-    cookie: {},
-    resave: false,
-    saveUninitialized: true,
-    store: new SequelizeStore({
-        db: sequelize
-    })
-};
+// const sess = {
+    //     secret: 'SuperSecret',
+    //     cookie: {},
+    //     resave: false,
+    //     saveUninitialized: true,
+    //     store: new SequelizeStore({
+        //         db: sequelize
+        //     })
+        // };
+        
+        // app.use(session(sess));
+        
+        // const helpers = require('./utils/helpers');
+        
+const hbs = exphbs.create({ });
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
-// app.use(session(sess));
-
-// const helpers = require('./utils/helpers');
-
-// const hbs = exphbs.create({ helpers });
-
-// app.engine('handlebars', hbs.engine);
-// app.set('view engine', 'handlebars');
-
+app.use(require('./controllers'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 sequelize.sync({ force: false }).then(() => {
