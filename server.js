fcfb3dbb259f64e7f7a79726fca478069c9a1aef
Asa Grace <asag182@gmail.com>
2021-09-28 00:34:20 -0600

    login-signup and logout

--- server.js
+++ server.js
@@ -2,27 +2,27 @@ const path = require('path');
 const express = require('express');
 const sequelize = require("./config/connection");
 
-// const session = require('express-session');
+const session = require('express-session');
 const exphbs = require('express-handlebars');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-// const sess = {
-    //     secret: 'SuperSecret',
-    //     cookie: {},
-    //     resave: false,
-    //     saveUninitialized: true,
-    //     store: new SequelizeStore({
-        //         db: sequelize
-        //     })
-        // };
+const sess = {
+    secret: 'SuperSecret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
         
-        // app.use(session(sess));
-        
-        // const helpers = require('./utils/helpers');
+app.use(session(sess));
+
+// const helpers = require('./utils/helpers');
         
 const hbs = exphbs.create({ });
 app.engine('handlebars', hbs.engine);
